5a81fa97cb5b552a99785488146591232a001f9f
jacksoncy <wxw198812@gmail.com>
2018-05-11 21:50:11 +0800

    add lsp

--- arch.sh
+++ arch.sh
@@ -1,1 +1,2 @@
 sudo pacman -S wqy-microhei texlive-latexextra texlive-langchinese texlive-bibtexextra texlive-fontsextra
+sudo npm i -g typescript javascript-typescript-langserver flow-language-server typescript-language-server
