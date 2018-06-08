7ac7b72317345a6896bd1ee65d270ab92ca4d1c8
jacksoncy <wxw2010@163.com>
2018-06-08 21:28:14 +0800

    update

--- npm.sh
+++ npm.sh
@@ -1,1 +1,1 @@
-sudo npm install -g eslint remark remark-lint-first-heading-level remark-lint typescript tslint typescript-formatter yaml-language-server tslint-language-service javascript-typescript-langserver vscode-html-languageservice vmd flow-language-server typescript-language-server
+sudo npm install -g eslint remark remark-lint-first-heading-level remark-lint typescript tslint typescript-formatter yaml-language-server tslint-language-service javascript-typescript-langserver vscode-html-languageservice flow-language-server typescript-language-server
