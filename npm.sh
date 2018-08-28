2fa8b9566f0fe24e44f965976e041ee55f4b768d
jacksoncy <wxw2010@163.com>
2018-08-28 21:23:40 +0800

    udpate

--- npm.sh
+++ npm.sh
@@ -1,1 +1,4 @@
-sudo npm install -g eslint remark remark-lint-first-heading-level remark-lint typescript tslint typescript-formatter yaml-language-server tslint-language-service javascript-typescript-langserver vscode-html-languageservice prettier
+sudo npm install -g eslint remark remark-lint-first-heading-level remark-lint typescript tslint 
+sudo npm install -g yaml-language-server tslint-language-service 
+sudo npm install -g javascript-typescript-langserver prettier vscode-json-languageservice
+sudo npm install -g vscode-html-languageserver-bin vscode-css-languageserver-bin
