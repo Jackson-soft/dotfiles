b692700b635135eb0c161587cdd8ed5651efc370
fusu <fusu@2dfire.com>
2019-05-29 10:52:12 +0800

    修改配置

--- cpcdm.sh
+++ cpcdm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp ./eslintrc.yml ~/.eslintrc.yml
+cp ./stylelintrc.json ~/.stylelintrc.json
 cp ./prettierrc.yml ~/.prettierrc.yml
 cp ./clang-format ~/.clang-format
 cp ./clang-tidy ~/.clang-tidy
--- npm.sh
+++ npm.sh
@@ -2,6 +2,7 @@
 
 npm i -g eslint markdownlint-cli markdownlint typescript
 npm i -g yaml-language-server typescript-language-server
-npm i -g vscode-html-languageserver-bin vscode-css-languageserver-bin
+npm i -g vscode-html-languageserver-bin vscode-css-languageserver-bin vscode-json-languageserver
 npm i -g vmd bash-language-server --unsafe-perm=true --allow-root
-npm i -g prettier @prettier/plugin-lua
+npm i -g prettier @prettier/plugin-lua stylelint-prettier stylelint eslint-plugin-prettier
+npm i -g @typescript-eslint/eslint-plugin @typescript-eslint/parser @typescript-eslint/typescript-estree
