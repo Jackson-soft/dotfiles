45de55a1aa11d7522e312a3911fa0baaa277f715
wuxw <wuxw@weipaitang.com>
2020-03-17 15:14:11 +0800

    修改命令错误

--- gocmd.sh
+++ gocmd.sh
@@ -7,10 +7,10 @@ go get golang.org/x/tools/gopls@latest
 go get github.com/stamblerre/gocode@latest
 go get github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 go get github.com/go-delve/delve/cmd/dlv@latest
-go get github.com/zmb3/gogetdoc@latest
 go get github.com/ramya-rao-a/go-outline@latest
-go get github.com/uudashr/gopkgs/cmd/gopkgs@latest
+go get github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
 go get github.com/josharian/impl@latest
 go get github.com/fatih/gomodifytags@latest
 go get github.com/acroca/go-symbols@latest
 go get github.com/davidrjenni/reftools/cmd/fillstruct@latest
+go get github.com/jessfraz/dockfmt@latest
