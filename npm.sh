43a8ba2e9d09a2eb247501a80e1a5ddbdf83f5e5
jacksoncy <wxw198812@gmail.com>
2020-06-04 19:52:57 +0800

    update config

--- npm.sh
+++ npm.sh
@@ -3,6 +3,6 @@
 npm i -g eslint markdownlint-cli markdownlint typescript
 npm i -g yaml-language-server typescript-language-server
 npm i -g vscode-html-languageserver-bin vscode-css-languageserver-bin vscode-json-languageserver
-npm i -g vmd bash-language-server --unsafe-perm=true --allow-root
+npm i -g bash-language-server sql-language-server
 npm i -g prettier @prettier/plugin-lua stylelint-prettier stylelint eslint-plugin-prettier
 npm i -g @typescript-eslint/eslint-plugin @typescript-eslint/parser @typescript-eslint/typescript-estree
