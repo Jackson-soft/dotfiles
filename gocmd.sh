56169af4319e5574d17529c948366b03bfa4ad6c
jacksoncy <wuxiangwei@yalla.live>
2021-06-10 15:46:10 +0800

    fix clang-format something

--- gocmd.sh
+++ gocmd.sh
@@ -4,4 +4,3 @@ go install golang.org/x/tools/gopls@latest
 go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
 go install github.com/fatih/gomodifytags@latest
-go install github.com/jessfraz/dockfmt@latest
