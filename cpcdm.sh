9d9aea53e8b388abbf948c9d18746ee627d4c4f6
jacksoncy <wuxiangwei@yalla.live>
2021-06-12 13:51:38 +0800

    imporve config

--- cpcdm.sh
+++ cpcdm.sh
@@ -3,5 +3,7 @@
 cp ./eslintrc.yaml ~/.eslintrc.yaml
 cp ./prettierrc.yaml ~/.prettierrc.yaml
 cp ./stylelintrc.yaml ~/.stylelintrc.yaml
-cp ./clang-format ~/.clang-format
-cp ./clang-tidy ~/.clang-tidy
+cp ./clang-format.yaml ~/.clang-format
+cp ./clang-tidy.yaml ~/.clang-tidy
+cp ./zshrc ~/.zshrc
+cp ./vimrc ~/.vimrc
