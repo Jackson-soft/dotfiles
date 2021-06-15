a01dc467ff4089af310ea8a25fbdb334825c5841
jacksoncy <wuxiangwei@yalla.live>
2021-06-15 14:23:00 +0800

    link config files

--- cpcdm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp ./eslintrc.yaml ~/.eslintrc.yaml
-cp ./prettierrc.yaml ~/.prettierrc.yaml
-cp ./stylelintrc.yaml ~/.stylelintrc.yaml
-cp ./clang-format.yaml ~/.clang-format
-cp ./clang-tidy.yaml ~/.clang-tidy
-cp ./zshrc ~/.zshrc
-cp ./vimrc ~/.vimrc
---
+++ link.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -
+
+set -o nounset # Treat unset variables as an error
+
+ln -s $(pwd)/zshrc $HOME/.zshrc
+ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd)/eslintrc.yaml $HOME/.eslintrc.yaml
+ln -s $(pwd)/prettierrc.yaml $HOME/.prettierrc.yaml
+ln -s $(pwd)/stylelintrc.yaml $HOME/.stylelintrc.yaml
+ln -s $(pwd)/clang-format.yaml $HOME/.clang-format
+ln -s $(pwd)/clang-tidy.yaml $HOME/.clang-tidy
