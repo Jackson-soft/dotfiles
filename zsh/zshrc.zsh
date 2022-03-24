53579ec86863ae20b05fcd4fe93079fb12469421
jacksoncy <wuxiangwei@yalla.live>
2022-03-24 16:55:44 +0800

    fix something

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,8 +1,9 @@
 ### Added by Zinit's installer
+local ZI_REPO="zdharma-continuum"
 if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
     print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
     command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
-    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+    command git clone https://github.com/$ZI_REPO/zinit "$HOME/.local/share/zinit/zinit.git" && \
         print -P "%F{33} %F{34}Installation successful.%f%b" || \
         print -P "%F{160} The clone has failed.%f%b"
 fi
@@ -12,49 +13,50 @@ autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 ### End of Zinit's installer chunk
 
-ZI_REPO="zdharma-continuum"
-zinit light-mode depth"1" for \
-    "$ZI_REPO"/zinit-annex-{'bin-gem-node','patch-dl'}
-
 # see https://thevaluable.dev/zsh-completion-guide-examples
-zinit wait lucid depth"1" light-mode for \
-    blockf \
-    atinit"
-        zstyle ':completion:*' completer _expand _complete _ignored _approximate
-        zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' '+r:|?=**'
-        zstyle ':completion:*' menu select=2
-        zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
-        zstyle ':completion:*:descriptions' format '[%d]'
-        zstyle ':completion:*:processes' command 'ps -au \$USER'
-        zstyle ':completion:*:*:*:*:processes' command 'ps -u \$USER -o pid,user,comm,cmd -w -w'
-        zstyle ':completion:*' file-sort modification
-        zstyle ':completion:*:git-checkout:*' sort false
-        zstyle ':completion:*' verbose yes
-        zstyle ':completion:*' squeeze-slashes true
-        zstyle ':completion:*' list-colors \${(s.:.)LS_COLORS}
-    " \
+zinit depth"1" light-mode for \
+    $ZI_REPO/zinit-annex-{'patch-dl','bin-gem-node'} \
+    blockf atpull'zinit creinstall -q .' \
+    atinit'
+        zstyle ":completion:*" completer _expand _complete _ignored _approximate
+        zstyle ":completion:*" matcher-list "m:{a-zA-Z}={A-Za-z}" "+r:|?=**"
+        zstyle ":completion:*" menu select=2
+        zstyle ":completion:*" select-prompt "%SScrolling active: current selection at %p%s"
+        zstyle ":completion:*:descriptions" format "[%d]"
+        zstyle ":completion:*:processes" command "ps -au $USER"
+        zstyle ":completion:*:*:*:*:processes" command "ps -u $USER -o pid,user,comm,cmd -w -w"
+        zstyle ":completion:*" file-sort modification
+        zstyle ":completion:*:git-checkout:*" sort false
+        zstyle ":completion:*" verbose yes
+        zstyle ":completion:*" squeeze-slashes true
+        zstyle ":completion:*" list-colors ${(s.:.)LS_COLORS}
+    ' \
         zsh-users/zsh-completions \
-    atinit"
-        zstyle ':fzf-tab:complete:(z|cd|exa):*' fzf-preview 'exa -1 --color=always \$realpath'
-        zstyle ':fzf-tab:complete:(\\|*/|)man:*' fzf-preview 'man \$word'
-        zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview 'git diff \$word | delta'
-        zstyle ':fzf-tab:complete:git-log:*' fzf-preview 'git log --color=always \$word'
-        zstyle ':fzf-tab:*' switch-group ',' '.'
-    " \
+    atinit'
+        zstyle ":fzf-tab:complete:(z|cd|exa):*" fzf-preview "exa -1 --color=always $realpath"
+        zstyle ":fzf-tab:complete:(\\|*/|)man:*" fzf-preview "man $word"
+        zstyle ":fzf-tab:complete:git-(add|diff|restore):*" fzf-preview "git diff $word | delta"
+        zstyle ":fzf-tab:complete:git-log:*" fzf-preview "git log --color=always $word"
+        zstyle ":fzf-tab:*" switch-group "," "."
+    ' \
         Aloxaf/fzf-tab \
     atinit"ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20;" atload"_zsh_autosuggest_start" \
         zsh-users/zsh-autosuggestions \
-    atinit"typeset -gA FAST_HIGHLIGHT; FAST_HIGHLIGHT[git-cmsg-len]=100; zicompinit; zicdreplay" \
-        "$ZI_REPO"/fast-syntax-highlighting \
-    trackbinds bindmap"^R -> ^H" atinit"
+    atinit'
+        typeset -gA FAST_HIGHLIGHT
+        FAST_HIGHLIGHT[git-cmsg-len]=100
+        zicompinit; zicdreplay
+    ' \
+        $ZI_REPO/fast-syntax-highlighting \
+    trackbinds bindmap"^R -> ^H" atinit'
         zstyle :history-search-multi-word page-size 20
         zstyle :history-search-multi-word highlight-color fg=red,bold
         zstyle :plugin:history-search-multi-word reset-prompt-protect 1
-    " \
-        "$ZI_REPO"/history-search-multi-word
+    ' \
+        $ZI_REPO/history-search-multi-word
 
 # git extensions
-zinit lucid wait"0a" depth"1" for \
+zinit wait"0a" lucid depth"1" for \
     as"program" pick"$ZPFX/bin/git-*" src"etc/git-extras-completion.zsh" make"PREFIX=$ZPFX" tj/git-extras \
     wfxr/forgit
 
@@ -90,16 +92,9 @@ zinit light junegunn/fzf
 zinit ice as"completion"
 zinit snippet https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker
 
-zinit light-mode lucid wait has"kubectl" for \
-    id-as"kubectl-completion" \
-    as"completion" \
-    atclone"kubectl completion zsh > _kubectl" \
-    atpull"%atclone" \
-    "$ZI_REPO"/null
-
-local dotHome=${HOME}/myDoc/dotfiles
-source ${dotHome}/zsh/conf.zsh
+local dotHome=$HOME/myDoc/dotfiles
+source $dotHome/zsh/conf.zsh
 # fzf
-source ${dotHome}/zsh/fzf.zsh
+source $dotHome/zsh/fzf.zsh
 
 export PATH=/usr/local/opt/llvm/bin:$PATH:$HOME/go/bin
