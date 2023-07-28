526e242b2b1889369f257be177830f43e3049549
jacksoncy <wuxiangwei@yalla.com>
2023-07-28 16:40:50 +0800

    add file brower key bind

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -139,11 +139,13 @@ require("lazy").setup({
             vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
             vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
             vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find existing buffers' })
+            vim.keymap.set('n', '<leader>eb', builtin.buffers, { desc = 'Find existing buffers' })
             vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
             vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, { desc = "[D]ocument [S]ymbols" })
             vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
             vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+            vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>", { noremap = true, desc = "file browser" }
+            )
         end,
         dependencies = {
             { "nvim-telescope/telescope-fzf-native.nvim",   build = 'make' },
