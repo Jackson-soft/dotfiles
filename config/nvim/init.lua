406162f70e8d61be410731e2bd46080f165d4a53
jacksoncy <wxw198812@gamil.com>
2024-09-04 01:05:04 +0800

    update tree-sitter

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -141,125 +141,130 @@ require("lazy").setup({
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        opts = {
-            ensure_installed = {
-                "bash",
-                "c",
-                "cmake",
-                "comment",
-                "cpp",
-                "css",
-                "dockerfile",
-                "dot",
-                "doxygen",
-                "diff",
-                "git_config",
-                "gitignore",
-                "go",
-                "gomod",
-                "gosum",
-                "gowork",
-                "html",
-                "http",
-                "javascript",
-                "json",
-                "jsonc",
-                "vim",
-                "vimdoc",
-                "lua",
-                "make",
-                "markdown",
-                "org",
-                "proto",
-                "python",
-                "regex",
-                "sql",
-                "toml",
-                "typescript",
-                "yaml",
-            },
-            highlight = { enable = true },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "gnn",
-                    node_incremental = "grn",
-                    scope_incremental = "grc",
-                    node_decremental = "grm",
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                ensure_installed = {
+                    "bash",
+                    "c",
+                    "cmake",
+                    "comment",
+                    "cpp",
+                    "css",
+                    "dockerfile",
+                    "dot",
+                    "doxygen",
+                    "diff",
+                    "git_config",
+                    "gitignore",
+                    "go",
+                    "gomod",
+                    "gosum",
+                    "gowork",
+                    "html",
+                    "http",
+                    "javascript",
+                    "json",
+                    "jsonc",
+                    "vim",
+                    "vimdoc",
+                    "lua",
+                    "make",
+                    "markdown",
+                    "org",
+                    "proto",
+                    "python",
+                    "regex",
+                    "sql",
+                    "toml",
+                    "typescript",
+                    "yaml",
                 },
-            },
-            indent = { enable = true },
-            refactor = {
-                highlight_definitions = { enable = true, clear_on_cursor_move = true },
-                smart_rename = {
+                sync_install = true,
+                auto_install = true,
+                highlight = { enable = true },
+                incremental_selection = {
                     enable = true,
                     keymaps = {
-                        smart_rename = "grr",
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
                     },
                 },
-                navigation = {
-                    enable = true,
-                    keymaps = {
-                        goto_definition = "gnd",
-                        list_definitions = "gnD",
-                        list_definitions_toc = "gO",
-                        goto_next_usage = "<a-*>",
-                        goto_previous_usage = "<a-#>",
+                indent = { enable = true },
+                refactor = {
+                    highlight_definitions = { enable = true, clear_on_cursor_move = true },
+                    smart_rename = {
+                        enable = true,
+                        keymaps = {
+                            smart_rename = "grr",
+                        },
+                    },
+                    navigation = {
+                        enable = true,
+                        keymaps = {
+                            goto_definition = "gnd",
+                            list_definitions = "gnD",
+                            list_definitions_toc = "gO",
+                            goto_next_usage = "<a-*>",
+                            goto_previous_usage = "<a-#>",
+                        },
                     },
                 },
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    -- Automatically jump forward to textobj, similar to targets.vim
-                    lookahead = true,
-                    keymaps = {
-                        -- You can use the capture groups defined in textobjects.scm
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
+                textobjects = {
+                    select = {
+                        enable = true,
+                        -- Automatically jump forward to textobj, similar to targets.vim
+                        lookahead = true,
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = "@class.inner",
+                        },
+                    },
+                    swap = {
+                        enable = true,
+                        swap_next = {
+                            ["<leader>a"] = "@parameter.inner",
+                        },
+                        swap_previous = {
+                            ["<leader>A"] = "@parameter.inner",
+                        },
+                    },
+                    move = {
+                        enable = true,
+                        set_jumps = true, -- whether to set jumps in the jumplist
+                        goto_next_start = {
+                            ["]m"] = "@function.outer",
+                            ["]]"] = "@class.outer",
+                        },
+                        goto_next_end = {
+                            ["]M"] = "@function.outer",
+                            ["]["] = "@class.outer",
+                        },
+                        goto_previous_start = {
+                            ["[m"] = "@function.outer",
+                            ["[["] = "@class.outer",
+                        },
+                        goto_previous_end = {
+                            ["[M"] = "@function.outer",
+                            ["[]"] = "@class.outer",
+                        },
+                    },
+                    lsp_interop = {
+                        enable = true,
+                        border = "none",
+                        peek_definition_code = {
+                            ["df"] = "@function.outer",
+                            ["dF"] = "@class.outer",
+                        },
                     },
                 },
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        ["<leader>a"] = "@parameter.inner",
-                    },
-                    swap_previous = {
-                        ["<leader>A"] = "@parameter.inner",
-                    },
-                },
-                move = {
-                    enable = true,
-                    set_jumps = true, -- whether to set jumps in the jumplist
-                    goto_next_start = {
-                        ["]m"] = "@function.outer",
-                        ["]]"] = "@class.outer",
-                    },
-                    goto_next_end = {
-                        ["]M"] = "@function.outer",
-                        ["]["] = "@class.outer",
-                    },
-                    goto_previous_start = {
-                        ["[m"] = "@function.outer",
-                        ["[["] = "@class.outer",
-                    },
-                    goto_previous_end = {
-                        ["[M"] = "@function.outer",
-                        ["[]"] = "@class.outer",
-                    },
-                },
-                lsp_interop = {
-                    enable = true,
-                    border = "none",
-                    peek_definition_code = {
-                        ["df"] = "@function.outer",
-                        ["dF"] = "@class.outer",
-                    },
-                },
-            },
-        },
+            })
+        end,
         dependencies = {
             { "nvim-treesitter/nvim-treesitter-context" },
             { "nvim-treesitter/nvim-treesitter-refactor" },
