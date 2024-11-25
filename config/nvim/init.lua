fc39ea18dfee3f5863c0e23994c1d3b39fc7a001
jacksoncy <wuxiangwei@yalla.com>
2024-11-25 18:30:14 +0800

    update

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -403,7 +403,7 @@ require("lazy").setup({
         'saghen/blink.cmp',
         lazy = false, -- lazy loading handled internally
         -- optional: provides snippets for the snippet source
-        dependencies = 'rafamadriz/friendly-snippets',
+        dependencies = { "rafamadriz/friendly-snippets" },
 
         -- use a release tag to download pre-built binaries
         version = 'v0.*',
@@ -433,16 +433,13 @@ require("lazy").setup({
             -- your own keymap.
             keymap = { preset = 'enter' },
 
-            -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- adjusts spacing to ensure icons are aligned
-            nerd_font_variant = 'normal',
-
             -- experimental auto-brackets support
             accept = { auto_brackets = { enabled = true } },
 
             -- experimental signature help support
             trigger = { signature_help = { enabled = true } }
-        }
+        },
+        opts_extend = { "sources.completion.enabled_providers" }
     },
 
     {
