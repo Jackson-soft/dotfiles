6a998f21b6a8d40bafb8cfc8d6704f46c1d0aceb
jacksoncy <wuxiangwei@yalla.com>
2025-04-25 11:29:10 +0800

    update lua_ls

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -567,17 +567,12 @@ require("lazy").setup({
                             callSnippet = 'Replace',
                             displayContext = 1,
                         },
+                        diagnostics = {
+                            globals = { 'vim' },
+                        },
                         -- Make the server aware of Neovim runtime files
                         workspace = {
-                            checkThirdParty = false,
-                            library = {
-                                vim.env.VIMRUNTIME
-                                -- Depending on the usage, you might want to add additional paths here.
-                                -- "${3rd}/luv/library"
-                                -- "${3rd}/busted/library",
-                            }
-                            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-                            -- library = vim.api.nvim_get_runtime_file("", true)
+                            library = vim.api.nvim_get_runtime_file("", true),
                         },
                         telemetry = {
                             enable = false,
