84af842675cf5d80f782968454dbe83a155faf52
jacksoncy <wxw198812@gmail.com>
2025-08-27 16:50:45 +0800

    improve

--- program/fedora.sh
+++ program/fedora.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-dnf upgrade --refresh
+dnf upgrade -y --refresh
 
 # enable the RPM Fusion software repositories
-dnf install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+dnf install -y "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
 # install vscode, edge
 dnf config-manager setopt fastestmirror=1
